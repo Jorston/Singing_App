515249cbe1c8d5bb4821be3cd13c8f8b8f26941e
jorge <donjuan.jfc@gmail.com>
2020-02-21 23:24:56 +0100

    implements method delete for PSQL userNick in fragment delete

--- app/src/main/java/com/example/app/Interfaces/Delete.java
+++ app/src/main/java/com/example/app/Interfaces/Delete.java
@@ -1,18 +1,35 @@
 package com.example.app.Interfaces;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.app.ConexionPSQL.ConexionPsql;
 import com.example.app.R;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 
 public class Delete extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
+    Button btnEliminar;
+
+    TextView nickEliminar;
+
+    View vista;
+
+    boolean interruptor;
 
     public Delete() {
         // Required empty public constructor
@@ -36,14 +53,97 @@ public class Delete extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delete, container, false);
+        vista = inflater.inflate(R.layout.fragment_delete, container, false);
+
+        btnEliminar = vista.findViewById(R.id.botonDelete);
+
+        nickEliminar = vista.findViewById(R.id.textnickDelete);
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //instacia de postgres
+                final hiloEliminarnick hiloEliminacion = new hiloEliminarnick(nickEliminar.getText().toString());
+
+                hiloEliminacion.execute();
+            }
+        });
+
+
+
+        return vista;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    //clase multitarea
+    public class hiloEliminarnick extends AsyncTask<String,Void,String> {
+
+        private String nickUsuario;
+
+        public hiloEliminarnick(String nickUsuario) {
+            this.nickUsuario = nickUsuario;
         }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected String doInBackground(String... strings) {
+
+            //conexion para PSQL Instanciamos objetos
+            ConexionPsql conexionPsql = new ConexionPsql();
+
+            Connection con = null;
+
+            con = conexionPsql.conectar();
+
+            //si coneccion insertamos en PSQL
+            if (con != null){
+
+                try {
+
+                    String deleteUser = "DELETE FROM userspostsql where usernick = ? ;";
+
+                    PreparedStatement eliminarUser;
+
+                    eliminarUser = con.prepareStatement(deleteUser);
+
+                    eliminarUser.setString(1,nickEliminar.getText().toString());
+
+                    eliminarUser.executeUpdate();
+
+                    interruptor = true;
+
+
+                } catch (SQLException e) {
+
+                    e.printStackTrace();
+                }
+            }else{
+                
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String s) {
+
+            super.onPostExecute(s);
+
+            if (interruptor){
+
+                showMessage("Usuario eliminado");
+
+            }else{
+
+                showMessage("no existe el usuario");
+            }
+
+        }
+    }
+
+    //metodo atajo para el toast vista usuario
+    protected void showMessage(String message){
+
+        Toast.makeText(getActivity(),message,Toast.LENGTH_SHORT).show();
     }
 
     @Override
