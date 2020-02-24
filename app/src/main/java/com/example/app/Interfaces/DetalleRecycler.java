f107455cfa1eba3fb59bcb0cc93ccd6394e587a6
jorge <donjuan.jfc@gmail.com>
2020-02-25 00:00:57 +0100

    activity creation recyclerview item details files and methods adapter

---
+++ app/src/main/java/com/example/app/Interfaces/DetalleRecycler.java
@@ -0,0 +1,30 @@
+package com.example.app.Interfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.example.app.R;
+
+public class DetalleRecycler extends AppCompatActivity {
+
+    TextView textoEjempo;
+
+    String value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detalle_recycler);
+
+        textoEjempo = (TextView) findViewById(R.id.textDescripcion);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            value = extras.getString("envio");
+        }
+
+        textoEjempo.setText(value);
+    }
+}
--- app/src/main/java/com/example/app/ModelosAdaptadores/AdaptadorRoomFichaje.java
+++ app/src/main/java/com/example/app/ModelosAdaptadores/AdaptadorRoomFichaje.java
@@ -1,17 +1,21 @@
 package com.example.app.ModelosAdaptadores;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.app.ConexionesRoom.FichajeRoom;
+import com.example.app.Interfaces.DetalleRecycler;
 import com.example.app.R;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-public class AdaptadorRoomFichaje extends RecyclerView.Adapter<AdaptadorRoomFichaje.ListadoRoomDBViewHolder> {
+public class AdaptadorRoomFichaje extends RecyclerView.Adapter<AdaptadorRoomFichaje.ListadoRoomDBViewHolder>{
 
     //variables globales
     List<FichajeRoom> listadoFichajesRoom;
@@ -42,23 +46,58 @@ public class AdaptadorRoomFichaje extends RecyclerView.Adapter<AdaptadorRoomFich
         holder.horaRoom.setText(listadoFichajesRoom.get(position).getHorafichaje());
 
         holder.tipoFichajeRoom.setText(listadoFichajesRoom.get(position).getTipoFichaje());
+
+        //set eventos implementamos metodo para poder abrir detalles de cada elemento
+        holder.setOnClickListeners();
     }
 
     @Override
     public int getItemCount() { return listadoFichajesRoom.size(); }
 
-    public class ListadoRoomDBViewHolder extends RecyclerView.ViewHolder {
+    public class ListadoRoomDBViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView horaRoom,diaRoom,tipoFichajeRoom;
 
+        Button botonDetalles;
+
+        Context context;
+
         public ListadoRoomDBViewHolder(@NonNull View itemView) {
+
             super(itemView);
+
+            context = itemView.getContext();
 
             diaRoom = (TextView) itemView.findViewById(R.id.fechaEntradaRoom);
 
             horaRoom = (TextView) itemView.findViewById(R.id.horaEntradaRoom);
 
             tipoFichajeRoom = (TextView) itemView.findViewById(R.id.tipoHorarioRoom);
+
+            //implementamos boton para ver detalles de elemento
+
+            botonDetalles = (Button) itemView.findViewById(R.id.btnDetalles);
+        }
+
+        void setOnClickListeners(){
+            botonDetalles.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()){
+
+                case R.id.btnDetalles:
+
+                    Intent intent = new Intent(context, DetalleRecycler.class);
+
+                    intent.putExtra("envio",tipoFichajeRoom.getText());
+
+                    context.startActivity(intent);
+
+                    break;
+            }
         }
     }
 }
