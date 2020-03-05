0619855ee713e19e98c9c25445a30360e633fa0e
jorge <donjuan.jfc@gmail.com>
2020-03-06 00:33:54 +0100

    Refactoring entorn for admin to fragment to activity

---
+++ app/src/main/java/com/example/app/Interfaces/ListadoDepartamentos.java
@@ -0,0 +1,72 @@
+package com.example.app.Interfaces;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.app.R;
+
+public class ListadoDepartamentos extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ListadoDepartamentos() {
+        // Required empty public constructor
+    }
+
+    public static ListadoDepartamentos newInstance(String param1, String param2) {
+        ListadoDepartamentos fragment = new ListadoDepartamentos();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View vista = inflater.inflate(R.layout.fragment_listado_departamentos, container, false);
+        return vista;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
