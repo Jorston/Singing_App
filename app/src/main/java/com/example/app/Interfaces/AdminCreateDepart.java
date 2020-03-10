f24d2305d4c9f24c7aa3caded5abfc4a9afab777
jorge <donjuan.jfc@gmail.com>
2020-03-10 19:27:44 +0100

    create branchs for depart and users in farebase

--- app/src/main/java/com/example/app/Interfaces/AdminCreateDepart.java
+++ app/src/main/java/com/example/app/Interfaces/AdminCreateDepart.java
@@ -5,10 +5,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.example.app.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AdminCreateDepart extends Fragment {
+
+    DatabaseReference mRootReference;
+
+    Button btnGuardarDepart;
+
+    TextView textoDepartamento;
 
     public AdminCreateDepart() {
         // Required empty public constructor
@@ -33,6 +47,36 @@ public class AdminCreateDepart extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_admin_create_depart, container, false);
+
+        textoDepartamento = vista.findViewById(R.id.txtDepart);
+
+        mRootReference = FirebaseDatabase.getInstance().getReference();
+
+        btnGuardarDepart = vista.findViewById(R.id.btndepartfirebase);
+
+        btnGuardarDepart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                crearDepartamento();
+            }
+        });
+
+
+
+
+
         return vista;
+    }
+
+    private void crearDepartamento() {
+
+        String parte = textoDepartamento.getText().toString();
+
+        Map<String, Object> datosDepart = new HashMap<>();
+
+       datosDepart.put("nombre","hola mundo");
+
+        mRootReference.child("Departamentoooos").child(parte).push().setValue(datosDepart);
     }
 }
