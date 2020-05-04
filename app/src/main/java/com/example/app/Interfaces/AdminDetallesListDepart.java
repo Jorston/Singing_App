3e7eed6d4c769b14ead499c6aebcbc49c57bbf52
jorge <donjuan.jfc@gmail.com>
2020-05-04 21:35:20 +0200

    refactory interface lists detalls departs

--- app/src/main/java/com/example/app/Interfaces/AdminDetallesListDepart.java
+++ app/src/main/java/com/example/app/Interfaces/AdminDetallesListDepart.java
@@ -4,11 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.widget.TextView;
 import com.example.app.ConexionMetodosFirebase.AdminDepartDetalles;
-import com.example.app.ConexionMetodosFirebase.UserMensajeFirebase;
 import com.example.app.ModelosAdaptadores.AdaptadorDetallesDepartFirebase;
 import com.example.app.R;
 import com.google.firebase.database.DataSnapshot;
@@ -16,7 +14,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 
 public class AdminDetallesListDepart extends AppCompatActivity {
